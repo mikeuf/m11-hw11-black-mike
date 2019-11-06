24d3a6d456f9ae604b8a7d0c86cce8ba62df1209
Mike <mblack101@ufl.edu>
2019-11-05 23:32:44 -0500

    Add event listener for boxes

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 //Global Varibales
+const myBoxes = document.getElementsByClassName('block');
 
 
 //The Functionality
+function loopThrough() {
+
+};
 
 
 //The Event Listener
+for (let i = 0, len = myBoxes.length; i < len; ++i) {
+myBoxes[i].addEventListener('click', loopThrough);
+};
